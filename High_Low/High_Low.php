60613fe87a565e59f5d51b278669526d8a78306a
Daniel Jimenez <danieljimenez@Daniels-MacBook-Air.local>
2014-05-12 14:59:17 -0500

    added $argv into the coding

--- High_Low/High_Low.php
+++ High_Low/High_Low.php
@@ -1,27 +1,34 @@
 <?php
 
-$number = rand ( 1 , 100 );
+$lownumber = $argv[1];
+
+$highnumber = $argv[2];
+
+$number = rand ( $lownumber , $highnumber );
 
 $attempts = 1;
 
-fwrite(STDOUT, "Pick a number between 1 and 100 \n");
+fwrite(STDOUT, "Pick a number between $lownumber and $highnumber" . PHP_EOL);
 
+if ($argc < 3) {
+	echo "Hey! I need you to put in more information!\n";
+	exit(1);
+}
 $guess = (int) fgets(STDIN);
 
 do 
 {
 	if ($guess < $number) 
 	{
-	
-		fwrite(STDOUT, "HIGHER\n");
+		fwrite(STDOUT, "HIGHER:\n");
 		$guess = (int) fgets(STDIN);
-	}
+	}	
 	elseif ($guess > $number)
 	{
-		fwrite(STDOUT, "LOWER\n" );
+		fwrite(STDOUT, "LOWER:\n" );
 		$guess = (int) fgets(STDIN);
 	}
-	echo "Guess? ";
+
 	$attempts++;
 } 
 while ($guess != $number);
