8201978c4efeec4c8d0bfe746b878921f9330acc
Daniel Jimenez <danieljimenez@Daniels-MacBook-Air.local>
2014-05-12 11:42:08 -0500

    updated High Low guessing game

--- High_Low/High_Low.php
+++ High_Low/High_Low.php
@@ -2,6 +2,8 @@
 
 $number = rand ( 1 , 100 );
 
+$attempts = 1;
+
 fwrite(STDOUT, 'Guess? ');
 
 $guess = (int) fgets(STDIN);
@@ -11,21 +13,23 @@ do
 	if ($guess < $number) 
 	{
 	
-		fwrite(STDOUT, 'HIGHER ');
+		fwrite(STDOUT, "HIGHER\n");
+		echo "Guess? ";
 		$guess = (int) fgets(STDIN);
 	}
 	
 	elseif ($guess > $number)
 	{
 		
-		fwrite(STDOUT, 'LOWER ');
+		fwrite(STDOUT, "LOWER\n" );
+		echo "Guess? ";
 		$guess = (int) fgets(STDIN);
 	}
-
+	$attempts++;
 } 
 while ($guess != $number);
 
-echo 'GOOD GUESS!';
+echo "GOOD GUESS, you are correct! It took you {$attempts} guesses.";
 	
 
 ?>
